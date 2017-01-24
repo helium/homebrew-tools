e960f3cc4737dce549e0fe9ab47a7ad799556b2c
Marc Nijdam <marc@helium.com>
2017-01-23 19:47:30 -0800

    Fix up to use TLS for s3

--- Formula/helium-script.rb
+++ Formula/helium-script.rb
@@ -4,7 +4,7 @@ class HeliumScript < Formula
   desc "A command line interface to Helium Script."
   homepage 'https://dev.helium.com/downloads'
   version '0.1.0'
-  url 'http://helium-public.s3.amazonaws.com/homebrew/bottle/helium-script-0.1.0.tar.gz'
+  url 'https://helium-public.s3.amazonaws.com/homebrew/bottle/helium-script-0.1.0.tar.gz'
   sha256 '8c377413d88ed29b658357cb17fb1ae7b269e0fd61fd19a30972beef0ce66dc8'
 
   def install
@@ -12,4 +12,3 @@ class HeliumScript < Formula
     system 'cp', '-r', 'bin', "#{prefix}/"
   end
 end
-
