b10650cfbf07730643bc7a0712143bbd4585e5c5
Marc Nijdam <marc@helium.com>
2017-01-31 13:39:01 -0800

    fixing sha for 1.0.0

--- Formula/helium-script.rb
+++ Formula/helium-script.rb
@@ -5,7 +5,7 @@ class HeliumScript < Formula
   homepage 'https://dev.helium.com/downloads'
   version '1.0.0'
   url 'http://helium-public.s3.amazonaws.com/homebrew/bottle/helium-script-1.0.0.tar.gz'
-  sha256 '58e6ffe9b97a081dcc6a9e547390c68f9b6932f836e199122d7a98350a623523'
+  sha256 '762cb34d253f8a9ad932f59b977dce0d36778f2004b79f7678b1f72d9c6f69ec'
 
   def install
     ohai 'Copying binaries...'
