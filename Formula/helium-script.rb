9eb7dfe7d7341805067b53fec684767d3b55cf58
Marc Nijdam <marc@helium.com>
2017-02-01 08:35:31 -0800

    helium-script 1.0.1

--- Formula/helium-script.rb
+++ Formula/helium-script.rb
@@ -3,9 +3,9 @@ require 'formula'
 class HeliumScript < Formula
   desc "A command line interface to Helium Script."
   homepage 'https://dev.helium.com/downloads'
-  version '1.0.0'
-  url 'http://helium-public.s3.amazonaws.com/homebrew/bottle/helium-script-1.0.0.tar.gz'
-  sha256 '762cb34d253f8a9ad932f59b977dce0d36778f2004b79f7678b1f72d9c6f69ec'
+  version '1.0.1'
+  url 'http://helium-public.s3.amazonaws.com/homebrew/bottle/helium-script-1.0.1.tar.gz'
+  sha256 'bd61518f54b7335f401300294a8c083adfbb366bb164462da21aab5be7bbdf95'
 
   def install
     ohai 'Copying binaries...'
